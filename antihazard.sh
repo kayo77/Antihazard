7a24374ddba736789cb90064af1c6319fa0b739d
kayo77 <kayo77@gmail.com>
2022-06-24 18:02:48 +0200

    Create antihazard.sh

---
+++ antihazard.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+curl -k --silent -H "Accept: application/xml" -H "Content-Type: application/xml" -X GET https://hazard.mf.gov.pl/api/Register | \
+     grep "AdresDomeny" | awk -F "<AdresDomeny>" '{print $2}' | \
+     idn2 | awk -F "</adresdomeny>" '{print  "zone \""$1"\" {type master; file \"/etc/bind/hazard.hosts\";};" }' > /tmp/hazard.zones
+ 
+/usr/sbin/named-checkzone /tmp/hazard.zones /etc/bind/hazard.hosts
+if [ $? -eq 0 ]
+then
+    cp -u --preserve=mode,ownership /tmp/hazard.zones /etc/bind/hazard.zones
+    /usr/sbin/rndc reload
+    rm /tmp/hazard.zones
+else
+    echo "ERROR hazard!"
+fi
+ 
+curl -k --silent -H "Accept: application/xml" -H "Content-Type: application/xml" -X GET https://hole.cert.pl/domains/domains.xml | \
+     grep "AdresDomeny" | awk -F "<AdresDomeny>" '{print $2}' | \
+     idn2 | awk -F "</adresdomeny>" '{print  "zone \""$1"\" {type master; file \"/etc/bind/certplbh.hosts\";};" }' > /tmp/certplbh.zones
+/usr/sbin/named-checkzone /tmp/certplbh.zones /etc/bind/certplbh.hosts
+if [ $? -eq 0 ]
+then
+    cp -u --preserve=mode,ownership /tmp/certplbh.zones /etc/bind/certplbh.zones
+    /usr/sbin/rndc reload
+    rm /tmp/certplbh.zones
+else
+    echo "ERROR cert!"
+fi
